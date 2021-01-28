2c2be7a2f581c1abcefa2ec43c141bc86978296b
Grociu <jan.grottel@gmail.com>
2021-01-28 14:38:33 +0100

    Add tests for the Fraction class logic operators

--- 001-fraction-arithmetic/tests/unittest.cpp
+++ 001-fraction-arithmetic/tests/unittest.cpp
@@ -168,6 +168,199 @@ TEST(FractionCreationTests, PrecisionTestingForFractionCreation)
 }
 
 /***********************************************************************
+THESE TESTS CHECK THE RESULTS OF THE FRACTION OPERATORS
+***********************************************************************/
+/***********************************************************************
+EQUAL/NOT EQUAL OPERATORS
+***********************************************************************/
+
+// test for equality true
+TEST(FractionEqualityOperatorTests, EqualIsTrue)
+{
+    const int test_cases_size = 6;
+    double test_cases[][4] =
+    {
+        {1, 2, 1, 2},  // numerator1, denominator1, denominator2, denominator2
+        {-5, -5, 1, 1},
+        {4, 8, 1, 2},  // simplified before comparison?
+        {1.5, 1, 12, 8},
+        {3, -7, -9, 21},
+        {0.25, 1, 1, 4} //size 6
+    };
+    for(int i = 0; i < test_cases_size; i++)
+    {
+        Fraction test_fraction_1 = Fraction(test_cases[i][0], test_cases[i][1]);
+        Fraction test_fraction_2 = Fraction(test_cases[i][2], test_cases[i][3]);
+        EXPECT_TRUE(test_fraction_1 == test_fraction_2) << "Equality test of index "
+        << i << " should be TRUE\nRaw data: " << test_cases[i][0] << " "
+        << test_cases[i][1] << " " << test_cases[i][2] << " " << test_cases[i][3]
+        << std::endl;
+    }
+}
+
+// tests for equality false
+TEST(FractionEqualityOperatorTests, EqualIsFalse)
+{
+    const int test_cases_size = 5;
+    double test_cases[][4] =
+    {
+        {1, 2, 1, 7},  // numerator1, denominator1, denominator2, denominator2
+        {-5, -5, -1, 1},
+        {1.5, 1, 1, 8},
+        {3, -7, 9, 21},
+        {0.25, 1, 1, 40} //size 5
+    };
+    for(int i = 0; i < test_cases_size; i++)
+    {
+        Fraction test_fraction_1 = Fraction(test_cases[i][0], test_cases[i][1]);
+        Fraction test_fraction_2 = Fraction(test_cases[i][2], test_cases[i][3]);
+        EXPECT_FALSE(test_fraction_1 == test_fraction_2) << "Equality test of index "
+        << i << " should be FALSE\nRaw data: " << test_cases[i][0] << " "
+        << test_cases[i][1] << " " << test_cases[i][2] << " " << test_cases[i][3]
+        << std::endl;
+    }
+}
+
+// tests for not-equal true
+TEST(FractionEqualityOperatorTests, NotEqualIsTrue)
+{
+    const int test_cases_size = 5;
+    double test_cases[][4] =
+    {
+        {1, 2, 1, 7},  // numerator1, denominator1, denominator2, denominator2
+        {-5, -5, -1, 1},
+        {1.5, 1, 1, 8},
+        {3, -7, 9, 21},
+        {0.25, 1, 1, 40} //size 5
+    };
+    for(int i = 0; i < test_cases_size; i++)
+    {
+        Fraction test_fraction_1 = Fraction(test_cases[i][0], test_cases[i][1]);
+        Fraction test_fraction_2 = Fraction(test_cases[i][2], test_cases[i][3]);
+        EXPECT_TRUE(test_fraction_1 != test_fraction_2) << "Noe-equal test of index "
+        << i << " should be TRUE\nRaw data: " << test_cases[i][0] << " "
+        << test_cases[i][1] << " " << test_cases[i][2] << " " << test_cases[i][3]
+        << std::endl;
+    }
+}
+
+// tests for not-equal false
+TEST(FractionEqualityOperatorTests, NotEqualIsFalse)
+{
+    const int test_cases_size = 6;
+    double test_cases[][4] =
+    {
+        {1, 2, 1, 2},  // numerator1, denominator1, denominator2, denominator2
+        {4, 8, 1, 2},  // simplified before comparison?
+        {-5, -5, 1, 1},
+        {1.5, 1, 12, 8},
+        {3, -7, -9, 21},
+        {0.25, 1, 1, 4} //size 5
+    };
+    for(int i = 0; i < test_cases_size; i++)
+    {
+        Fraction test_fraction_1 = Fraction(test_cases[i][0], test_cases[i][1]);
+        Fraction test_fraction_2 = Fraction(test_cases[i][2], test_cases[i][3]);
+        EXPECT_FALSE(test_fraction_1 != test_fraction_2) << "Not-equal test of index "
+        << i << " should be FALSE\nRaw data: " << test_cases[i][0] << " "
+        << test_cases[i][1] << " " << test_cases[i][2] << " " << test_cases[i][3]
+        << std::endl;
+    }
+}
+
+/***********************************************************************
+INEQUALITY OPERATORS
+***********************************************************************/
+
+TEST(FractionCompareTests, HalfIsLargerThanQuarter)
+{
+    Fraction test_fraction_1 = Fraction(1, 2);
+    Fraction test_fraction_2 = Fraction(1, 4);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, HalfIsLargerThanMinusQuarter)
+{
+    Fraction test_fraction_1 = Fraction(1, 2);
+    Fraction test_fraction_2 = Fraction(-1, 4);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, NegativeFractionisLargerThanAnother)
+{
+    Fraction test_fraction_1 = Fraction(-1, 4);
+    Fraction test_fraction_2 = Fraction(-1, 2);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, EighthIsLargerThanZero)
+{
+    Fraction test_fraction_1 = Fraction(1, 8);
+    Fraction test_fraction_2 = Fraction(0);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, ZeroIsLargerThanMinusQuarter)
+{
+    Fraction test_fraction_1 = Fraction(0);
+    Fraction test_fraction_2 = Fraction(-1, 8);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, DecimalFractionIsLargerThanAnother)
+{
+    Fraction test_fraction_1 = Fraction(0.57);
+    Fraction test_fraction_2 = Fraction(0.372);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+TEST(FractionCompareTests, NegativeDecimalFractionIsLargerThanAnother)
+{
+    Fraction test_fraction_1 = Fraction(-0.372);
+    Fraction test_fraction_2 = Fraction(-0.57);
+    EXPECT_TRUE(test_fraction_1 > test_fraction_2);
+}
+
+/***********************************************************************
+DERIVED INEQUALITY OPERATORS
+***********************************************************************/
+
+TEST(FractionCompareTests, HalfIsLargerOrEqualThanQuarter)
+{
+    Fraction test_fraction_1 = Fraction(1, 2);
+    Fraction test_fraction_2 = Fraction(1, 4);
+    EXPECT_TRUE(test_fraction_1 >= test_fraction_2);
+}
+
+TEST(FractionCompareTests, QuarterIsSmallerThanHalf)
+{
+    Fraction test_fraction_1 = Fraction(1, 4);
+    Fraction test_fraction_2 = Fraction(1, 2);
+    EXPECT_TRUE(test_fraction_1 < test_fraction_2);
+}
+
+TEST(FractionCompareTests, QuarterIsSmallerOrEqualThanHalf)
+{
+    Fraction test_fraction_1 = Fraction(1, 4);
+    Fraction test_fraction_2 = Fraction(1, 2);
+    EXPECT_TRUE(test_fraction_1 <= test_fraction_2);
+}
+
+TEST(FractionCompareTests, EqualsAreLargerOrEqual)
+{
+    Fraction test_fraction_1 = Fraction(1, 4);
+    Fraction test_fraction_2 = Fraction(1, 4);
+    EXPECT_TRUE(test_fraction_1 >= test_fraction_2);
+}
+
+TEST(FractionCompareTests, EqualsAreSmallerOrEqual)
+{
+    Fraction test_fraction_1 = Fraction(1, 4);
+    Fraction test_fraction_2 = Fraction(1, 4);
+    EXPECT_TRUE(test_fraction_1 <= test_fraction_2);
+}
+
+/***********************************************************************
 ***********************************************************************/
 
 // main function for a Google Test CMake implementation
